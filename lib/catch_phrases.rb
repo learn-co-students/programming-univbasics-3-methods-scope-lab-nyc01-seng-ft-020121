8d596320b1ab87d2f3ae40489837c74535554b60
Gus Bikos <gusbikos@gmail.com>
2021-01-12 21:30:08 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -26,5 +26,4 @@ describe "all_phrases" do
     expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
     expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
     expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end
 end
