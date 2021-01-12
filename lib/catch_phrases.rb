8166af8f23f8d9758d82cd3ba51ab523209356f0
Gus Bikos <gusbikos@gmail.com>
2021-01-12 21:36:50 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -21,9 +21,8 @@ def link
 end
 
 def all_phrases
-describe "all_phrases" do
-  it "puts out all of the previous catch phrases" do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts "It's-a me, Mario!"
+  puts "Thank you Mario! But Our Princess Is In Another Castle!"
+  puts "It's Dangerous To Go Alone! Take This."
 end
