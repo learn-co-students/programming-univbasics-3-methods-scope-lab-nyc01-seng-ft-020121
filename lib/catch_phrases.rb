cf70f8635b0433ba1b8d033ad01051da81b2638e
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-20 20:47:06 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "It's-a me, Mario!\n"
 end
 
+
 def toadstool
-  puts status
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This.\n"
+end
+
+def all_phrases
+  puts "#{mario}"
+  puts "#{toadstool}"
+  puts "#{link}"
 end
